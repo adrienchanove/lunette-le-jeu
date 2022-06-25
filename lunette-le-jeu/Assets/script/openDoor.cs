f0518741165a054e70fc8e9c2ea691335106ffab
pradoxal_IV <68543395+adrienchanove@users.noreply.github.com>
2022-06-25 17:29:10 +0200

    IA

---
+++ lunette-le-jeu/Assets/script/openDoor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openDoor : MonoBehaviour
+{
+    Animation anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("collision");
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.tag);
+        //anim.Play("openDoor");
+        if (collision.gameObject.tag == "Player")
+        {
+            //anim.Play("openDoor");
+        }
+        if (collision.gameObject.tag == "Player")
+        {
+            anim.Play();
+        }
+    }
+}
